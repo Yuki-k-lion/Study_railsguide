771ab036a1deaf1538e9c6e78e415318732b6311
yuki komatsu <yuki.k.lion@gmail.com>
2019-02-06 23:39:10 +0900

    refactor

--- blog_guide/app/models/article.rb
+++ blog_guide/app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true,length: {minimum: 5}
 end
+
+# ある記事を削除したら、その記事に関連付けられているコメントも
+# 一緒に削除する必要があります。そうしないと、
+# コメントがいつまでもデータベース上に残ってしまいます。
+# Railsでは関連付けにdependentオプションを指定することで
+# これを実現しています。
