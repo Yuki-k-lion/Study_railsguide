d6430940fb2b2ac385614b57e1c977c91c9c29eb
yuki komatsu <yuki.k.lion@gmail.com>
2019-02-06 22:02:10 +0900

    学習用なので、コミットの大きさは気にしないことに

--- blog_guide/config/routes.rb
+++ blog_guide/config/routes.rb
@@ -2,5 +2,18 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+# get '/patients/:id', to: 'patients#show', as: 'patient'
   resources :articles
+
+
+# コントローラを名前空間によってグループ化することもできます。
+# 最もよく使用される名前空間といえば、
+# 多数の管理用コントローラ群をまとめるAdmin::名前空間でしょう。
+# これらのコントローラをapp/controllers/adminディレクトリに配置し、
+# ルーティングでこれらをグループ化できます。
+  # namespace :admin do
+  #   resources :users
+  # end
+  
 end
